37fd3a8fda0fcb0c4bd68d646dcd08679e2849db
youngae-sa <leehso0305@naver.com>
2025-10-29 16:07:03 +0900

    c

---
+++ .history/src/components/WorkProject_20251029132537.vue
@@ -0,0 +1,20 @@
+<template>
+    
+    <h2 class="blind">Work Project</h2>
+    <div class="work_content">                                
+        <div class="content_inner">
+            <div class="title_all">
+                <h3>온통청년</h3>
+                <p class="sub_tit">온통청년 메인 디자인 개편</p>
+                <div class="desc">
+                    <p>메인</p>
+                    <p>100% 참여</p>
+                </div>
+                <a href="/youthcenter/newmain.html" class="viewMore">viewmore</a>
+            </div>
+            <div class="pc_img">
+                <img src="/src/assets/images/youthcenter.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
--- .history/src/components/WorkProject_20251029150617.vue
+++ .history/src/components/WorkProject_20251029150617.vue
@@ -18,21 +18,5 @@
                 </div>
             </div>
         </section>
-        <section>
-            <div class="content__inner">
-                <div class="title_all">
-                    <h3>온통청년</h3>
-                    <p class="sub_tit">온통청년 메인 디자인 개편</p>
-                    <div class="desc">
-                        <p>메인</p>
-                        <p>100% 참여</p>
-                    </div>
-                    <a href="https://youngae-sa.github.io/workProject/youthcenter/newMain.html" class="viewMore" target="_blank" title="새창">viewmore</a>
-                </div>
-                <div class="pc_img">
-                    <img src="/src/assets/images/youthcenter.png" alt="">
-                </div>
-            </div>
-        </section>
     </div>
 </template>
